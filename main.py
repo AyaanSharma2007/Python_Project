59b35d182d40ebfd70a117dbd64cfbcf3a56befe
pixelguy021 <85433845+pixelguy021@users.noreply.github.com>
2025-11-13 01:40:59 +0530

    Add and fixed question and answer mode
    To add edit mode and and fix edit issue

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import random
 import copy
-
+#issues 
+# do you want to exit again aanf again
+#some desgin issue
+#
 questions_db=["Enter your CGPN"]
 option_2_q_db={}
 ans_2_q_db={"Enter your CGPN":"4"}#for mutiple correct and single correct only (to use the quesiton string as a key)
@@ -10,37 +13,54 @@ def printQ(a):#can i use a lambda here
     print(a[:-2],end=" ")
 def question_mode():
     #2 modes see the quesitons sorted and input questions
-    N=int(input("Sele ct Mode:"))
+    N=int(input("Select Mode:"))
     if(N==1):   #display all the questions in the data base
         for index in range(len(questions_db)):
+            print("--------------------- Questions-------------------------------")
             print(f" {index+1}.",end="")
             printQ(questions_db[index])
             print(" ")
             print(f"({question_types.get(questions_db[index][-1])})")
-            print("----------------------------------------------------")
-            print(" ")
-        pass#display mode
-    if(N==2):
+            
+            if (questions_db[index][-1]=="S" or questions_db[index][-1]=="M"):# its is unable to get that tupple so either its not storing it or its being made
+                print("--------------------- Options-------------------------------")
+                option_list=option_2_q_db.get(questions_db[index])   
+                for j in range(len(option_list)):
+                    print(f"{j+1}. {option_list[j]}")
+            print("--------------------- Answers-------------------------------")
+            if (questions_db[index][-1]=="S" or questions_db[index][-1]=="M"):
+                ans_list=ans_2_q_db.get(questions_db[index])
+                for j in range(len(ans_list)):
+                    print(f"{j+1}. {ans_list[j]}")
+            else :
+                print(f"{ans_2_q_db.get(questions_db[index])}")    
+            print("----------------------------------------------------")    
+            print(" ")   
+    elif(N==2):
         # add a mode to edit questions too
         run="Y"
         while(run=="Y") :
-            question_type=input("Choose a Question Type :(N,S,M,A)")#add a fail safe for junk values
+            question_type=input("Choose a Question Type :(N,S,M,A)").upper()#add a fail safe for junk values
             question_text=input("Enter Question Text :")# a check for repeat questions
-            if(question_type=="S" or question_type=="M"):
+            question_marks=input("Enter Marks :")# a check for repeat questions
+            questions_db.append(question_text+question_type.upper())
+            if(question_type=="S" or question_type=="M"):# For S and M case 
                 no_of_options=int(input("Enter the Number of options"))
                 temp_options=[]
-                for i in range(no_of_options):
-                    print(f"Enter Option No.{i+1} ")
-                    temp_options[i]=input()
-                temp_options=tuple(temp_options)
-                option_2_q_db[question_text]=temp_options
-                no_of_ans=int(input("Enter the correct options"))
+                for i in range(no_of_options):#gets all of the option
+                    print("enter option num ",end="")
+                    print(i+1)
+                    option=input()
+                    temp_options.append(option)
+                options=(temp_options)
+                option_2_q_db[question_text+question_type.upper()]=options
+                no_of_ans=int(input("Enter the correct options"))   
                 temp_options=[]
-                for i in range(no_of_options):
+                for i in range(no_of_ans):# getes the correct options (as a tuple)(run a loop to check each value)
                     print(f"Enter Option No.{i+1} ")
-                    temp_options[i]=input()
-                temp_options=tuple(temp_options)
-                ans_2_q_db[question_text]=temp_options
+                    option=input()
+                    temp_options.append(option)
+                ans_2_q_db[question_text+question_type.upper()]=temp_options
             else:#keep the same for (Numberic case too)
                 temp_answer=input("Enter the correct answer :").lower()
                 ans_2_q_db[question_text]=temp_answer
@@ -52,11 +72,11 @@ def question_mode():
                     runt=0
             # code getting the answer part
                 else:
-                    print("Input a valid value")
-            questions_db.append(question_text+question_type.upper())
+                    print("Input a valid value")            
+            questions_db_marks.append(question_marks)
             print(" ")
             run=input("Do you want continue (Press Y for yes)").upper()
-    else:
+    else:#this is trigger need
         print("Please select a Valid mode ")
 def answer_mdoe():
     #make a deepcopy of all the questions
@@ -65,15 +85,49 @@ def answer_mdoe():
     question_random=[]
     marks_random=[]
     answer_submitted={}
-    while(len(question_non_random)):
+    while(len(question_non_random)):# makes a list of random questiions
         i=random.randint(0,len(question_non_random)-1)
         marks_random.append(marks_non_random[i])
         question_random.append(question_non_random[i])
         del question_non_random[i]
         del marks_non_random[i]
-    for i in range(len(question_random)):
-        print(f"Q{i+1}. {question_random[i][:-2]}")
-        print(question_types.get(question_random[i][-1]))    
-        ans=input("Enter your answer")#for n and A case
-        answer_submitted[question_random[i]]=ans
-1
+    for i in range(len(question_random)):# displays the questions and asks for answers
+        current_question=question_random[i]
+        print(f"Q{i+1}. {current_question[:-2]}")
+        print(f"{question_types.get(current_question[-1])}          Marks={marks_random[i]}")   
+        if(current_question[-1]=="N"or current_question[-1]=="A"):#for n and A case
+            ans=input("Enter your answer")
+            answer_submitted[current_question]=ans
+        else :# for s and M case
+            print(question_types.get(current_question[-1])) 
+            option_list=[]
+            option_list=option_2_q_db.get(current_question)#weird way the code is storting    
+            for j in range(len(option_list)):
+                print(f"{j+1}. {option_list[j]}")
+            temp_options=[]
+            for j in range(len(ans_2_q_db.get(current_question))): #for now you the correct number of answers
+                print(f"Enter Option No.{j+1} ")
+                temp_options.append(input())
+            temp_options=tuple(temp_options)
+            answer_submitted[current_question]=temp_options
+    #after all the questions are done show the answer
+    for k,v in answer_submitted.items():
+        print(f"Q{question_random.index((k))+1}. Your answer is {v} ({marks_random[i]})")
+        print(" ")
+    #  do you wish to submit 
+def main_menu():
+    Running=1
+    while(Running):
+        N=input("Select a Mode")
+        if (N=="1"):
+            print("========================Questions Mode===========================")
+            question_mode()
+        elif (N=="2"):
+            print("========================Answering Mode===========================")
+            answer_mdoe()
+        else:
+            print("Input a valid mode")
+            R=input("Do you want to exit ?").lower()
+        if (R=="y"):
+            Running=0
+main_menu()
